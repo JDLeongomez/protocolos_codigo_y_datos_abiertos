adcefff55e9d96f1822de6f3f9399226b13b26bc
Juan David Leongomez <juanleongomez@gmail.com>
2026-01-18 18:49:56 -0500

    Tablas iniciales

---
+++ borrar.R
@@ -0,0 +1,8 @@
+## 📲 Accede a las diapositivas en línea
+
+<!-- [![](img/presentation-qr.svg){fig-align="center" width="45%"}](https://jdleongomez.github.io/Presentacion_MetaCiencia/) -->
+  
+  ::: {.aside .center}
+![](https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg){width="22" style="vertical-align:middle; margin-right:.35em;"}Materiales (incluyendo imágenes y código de R) disponibles en [GitHub](https://github.com/JDLeongomez/protocolos_codigo_y_datos_abiertos)
+:::
+  
